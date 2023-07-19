a8e1f0da6adda071a7d373a911f5b2acd3c5fedd
yosilru <yosilru@gmail.com>
2023-07-19 17:06:57 -0500

    Contador de productos en el carrito

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
+import { ShoppingCartContext } from '../../Context'
+
 
 const Navbar = () => {
+  const context = useContext(ShoppingCartContext)
   const activeStyle = 'underline underline-offset-4'
   
 
@@ -99,7 +103,7 @@ const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          🛒0
+          🛒 {context.count}
         </li>
       </ul>
     </nav>
