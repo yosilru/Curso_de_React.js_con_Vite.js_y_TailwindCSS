1fa6e25f79e779eb3fd74765ef8320b694e6daa3
yosilru <yosilru@gmail.com>
2023-07-19 18:41:46 -0500

    heroicons con TailwindCSS

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,3 +1,4 @@
+import { PlusIcon } from '@heroicons/react/24/solid'
 import { useContext } from 'react'
 import { ShoppingCartContext } from '../../Context'
 
@@ -9,7 +10,7 @@ const Card = (data) => {
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]}  alt={data.data.title} />
                 <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={() => context.setCount(context.count + 1)}>
-                    +
+                <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
                 </div>
             </figure>
             <p className='flex justify-between'>
