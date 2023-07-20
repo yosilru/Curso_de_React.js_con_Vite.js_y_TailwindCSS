b323b39c8b3a6b83a128015744ca3b560198e456
yosilru <yosilru@gmail.com>
2023-07-19 19:41:44 -0500

    Maquetando el ProductDetail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,11 +4,17 @@ export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
     const [count, setCount] = useState(0)
+    const [isProductDetailOpen, setisProductDetailOpen] = useState(false)
+    const openProductDetail = () => setisProductDetailOpen(true)
+    const closeProductDetail = () => setisProductDetailOpen(false)
 
     return (
         <ShoppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen
         }}>
             {children}
         </ShoppingCartContext.Provider>
