365662a1787381a06f2e3621ad1060c212023482
yosilru <yosilru@gmail.com>
2023-07-20 20:05:52 -0500

    Agregando productos al carrito

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,10 +5,14 @@ import ProductDetail from '../ProductDetail'
 
 const Card = (data) => {
     const context = useContext(ShoppingCartContext)
-
     const showProduct = (productDetail) => {
         context.openProductDetail()
         context.setProductToShow(productDetail)
+    }
+
+    const addProductsToCart = (productData) => {
+        context.setCount(context.count + 1)
+        context.setCartProducts([...context.cartProducts, productData])
     }
 
     return (
@@ -17,7 +21,7 @@ const Card = (data) => {
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]}  alt={data.data.title} />
-                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={() => context.setCount(context.count + 1)}>
+                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={() => addProductsToCart(data.data)}>
                 <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
                 </div>
             </figure>
