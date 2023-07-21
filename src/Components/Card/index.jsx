dfe87433a0f51290e5a6aaeb24f044b88f9a646e
yosilru <yosilru@gmail.com>
2023-07-20 21:41:37 -0500

    SideMenu del carrito de compras

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -5,14 +5,18 @@ import ProductDetail from '../ProductDetail'
 
 const Card = (data) => {
     const context = useContext(ShoppingCartContext)
+    
     const showProduct = (productDetail) => {
         context.openProductDetail()
         context.setProductToShow(productDetail)
     }
 
-    const addProductsToCart = (productData) => {
+    const addProductsToCart = (event, productData) => {
+        event.stopPropagation()
         context.setCount(context.count + 1)
         context.setCartProducts([...context.cartProducts, productData])
+        context.openCheckoutSideMenu()
+        context.closeProductDetail()
     }
 
     return (
@@ -21,7 +25,7 @@ const Card = (data) => {
             <figure className='relative mb-2 w-full h-4/5'>
                 <span className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.data.images[0]}  alt={data.data.title} />
-                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={() => addProductsToCart(data.data)}>
+                <div className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1' onClick={(event) => addProductsToCart(event, data.data)}>
                 <PlusIcon className='h-6 w-6 text-black'></PlusIcon>
                 </div>
             </figure>
