4667ce184234a9a902f7c574c803cd3239c6ac9e
yosilru <yosilru@gmail.com>
2023-07-23 19:19:12 -0500

    Componente OrderCard

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
+import OrderCard from '../../OrderCard'
 import './styles.css'
 
 const CheckoutSideMenu = () => {
@@ -15,6 +16,18 @@ const CheckoutSideMenu = () => {
                     <XMarkIcon className='h-6 w-6 text-black cursor-pointer'
                     onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
                 </div>
+            </div>
+            <div className='px-6'>
+                {
+                    context.cartProducts.map(product => (
+                        <OrderCard 
+                            key={product.id}
+                            title={product.title}
+                            imageUrl={product.images}
+                            price={product.price}
+                        />
+                    ))
+                }
             </div>
         </aside>
     )
