b778a1631d5eef47ee2c2d7b776ddbe0cc656b9f
yosilru <yosilru@gmail.com>
2023-07-26 20:09:26 -0500

    Eliminar productos del carrito

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -6,6 +6,11 @@ import './styles.css'
 
 const CheckoutSideMenu = () => {
     const context = useContext(ShoppingCartContext)
+
+    const handleDelete = (id) => {
+        const filteredProducts = context.cartProducts.filter(product => product.id != id)
+        context.setCartProducts(filteredProducts)
+    }
     
     return (
         <aside 
@@ -22,9 +27,11 @@ const CheckoutSideMenu = () => {
                     context.cartProducts.map(product => (
                         <OrderCard 
                             key={product.id}
+                            id={product.id}
                             title={product.title}
                             imageUrl={product.images}
                             price={product.price}
+                            handleDelete={handleDelete}
                         />
                     ))
                 }
