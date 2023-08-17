a52d4e052a5f3c8d84b5358846c8eb6d1857c4dd
yosilru <yosilru@gmail.com>
2023-08-16 19:02:08 -0500

    Filtrando categorías con JavaScript

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -20,6 +20,7 @@ const Navbar = () => {
         <li>
           <NavLink
             to='/'
+            onClick={() => context.setSearchByCategory()}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -29,6 +30,7 @@ const Navbar = () => {
         <li>
           <NavLink 
             to='/clothes'
+            onClick={() => context.setSearchByCategory('clothes')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -38,6 +40,7 @@ const Navbar = () => {
         <li>
           <NavLink 
             to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -47,6 +50,7 @@ const Navbar = () => {
         <li>
           <NavLink 
             to='/furnitures'
+            onClick={() => context.setSearchByCategory('furnitures')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -56,6 +60,7 @@ const Navbar = () => {
         <li>
           <NavLink 
             to='/toys'
+            onClick={() => context.setSearchByCategory('toys')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
@@ -65,6 +70,7 @@ const Navbar = () => {
         <li>
           <NavLink 
             to='/others'
+            onClick={() => context.setSearchByCategory('others')}
             className={({ isActive }) =>
               isActive ? activeStyle : undefined
             }>
